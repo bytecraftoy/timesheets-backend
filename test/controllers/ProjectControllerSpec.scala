828de6ade70b09e93f873259c62cc85ccd5328fa
Elias <manneli45@gmail.com>
2021-03-20 02:17:16 +0200

    TEST/US-34: add tests for adding and updating projects

--- test/controllers/ProjectControllerSpec.scala
+++ test/controllers/ProjectControllerSpec.scala
@@ -6,12 +6,10 @@ import org.scalatestplus.play.guice._
 import play.api.Logging
 import play.api.inject.Injector
 import play.api.inject.guice.GuiceApplicationBuilder
-import play.api.test._
+import play.api.libs.json.{JsValue, Json}
 import play.api.test.Helpers._
-import play.api.libs.json.Json
-import play.api.libs.json.JsValue
+import play.api.test._
 
-import javax.inject.Inject
 import scala.reflect.ClassTag
 
 object Inject {
@@ -157,6 +155,103 @@ class ProjectControllerSpec
         contentAsString(resultGet).contains("Can this project be retrieved?")
 
       bodyTextContainsPostedDescription mustEqual true
+    }
+
+    "record the specified Users as Employees in the Project" in {
+      val jsonString =
+        """{"name": "Test Project with Employees",
+          |"description": "This project should have more than zero employees",
+          |"client": "1bb44a7e-cd7c-447d-a9e9-26495b52fa88",
+          |"owner": "9fa407f4-7375-446b-92c6-c578839b7780",
+          |"employees": ["06be4b85-8f65-4f65-8965-faba1216f199",
+          |"4276164d-d8c3-47d5-8f65-a6255ce71567",
+          |"618d79bd-5afd-4f82-b265-729eb7b7e9f0",
+          |"c7af4280-7cc0-45aa-84ad-15156a17663e"],
+          |"billable": true
+          |}""".stripMargin
+      val json = Json.parse(jsonString)
+
+      val requestPost = FakeRequest(POST, "/projects")
+        .withHeaders("Content-type" -> "application/json")
+        .withBody[JsValue](json)
+
+      val resultPost = route(app, requestPost).get
+      status(resultPost) mustBe OK
+      contentType(resultPost) mustBe Some("application/json")
+      projectRepository.all.exists(
+        project =>
+          project.name == "Test Project with Employees" && project.employees.size == 4
+      )
+    }
+
+    "reject a Project whose owner is also in the employees list" in {
+      val jsonString =
+        """{"name": "Test Project",
+          |"description": "Trying to insert the owner as employee",
+          |"client": "1bb44a7e-cd7c-447d-a9e9-26495b52fa88",
+          |"owner": "9fa407f4-7375-446b-92c6-c578839b7780",
+          |"employees": ["9fa407f4-7375-446b-92c6-c578839b7780"],
+          |"billable": true
+          |}""".stripMargin
+      val json = Json.parse(jsonString)
+
+      val request = FakeRequest(POST, "/projects")
+        .withHeaders("Content-type" -> "application/json")
+        .withBody[JsValue](json)
+
+      val result = route(app, request).get
+      status(result) mustBe BAD_REQUEST
+      contentType(result) mustBe Some("application/json")
+    }
+  }
+
+  "ProjectController PUT" should {
+    "successfully update a Project" in {
+      val jsonString =
+        """{"name": "Test Project to update",
+          |"description": "Can this project be updated?",
+          |"client": "1bb44a7e-cd7c-447d-a9e9-26495b52fa88",
+          |"owner": "9fa407f4-7375-446b-92c6-c578839b7780",
+          |"employees": [],
+          |"billable": true
+          }""".stripMargin
+      val json = Json.parse(jsonString)
+
+      val requestPost = FakeRequest(POST, "/projects")
+        .withHeaders("Content-type" -> "application/json")
+        .withBody[JsValue](json)
+
+      val resultPost = route(app, requestPost).get
+      status(resultPost) mustBe OK
+      contentType(resultPost) mustBe Some("application/json")
+
+      val projectUuid =
+        projectRepository.all
+          .filter(_.name == "Test Project to update")
+          .head
+          .id
+
+      val jsonStringPut =
+        s"""{"id": "$projectUuid",
+          |"name": "Test Project, updated",
+          |"description": "Was this project updated?",
+          |"client": "1bb44a7e-cd7c-447d-a9e9-26495b52fa88",
+          |"owner": "9fa407f4-7375-446b-92c6-c578839b7780",
+          |"employees": ["c7af4280-7cc0-45aa-84ad-15156a17663e"],
+          |"billable": true
+          }""".stripMargin
+      val jsonPut = Json.parse(jsonStringPut)
+
+      val requestPut = FakeRequest(PUT, "/projects")
+        .withHeaders("Content-type" -> "application/json")
+        .withBody[JsValue](jsonPut)
+
+      val resultPut = route(app, requestPut).get
+      status(resultPut) mustBe OK
+      contentType(resultPut) mustBe Some("application/json")
+
+      projectRepository.byId(projectUuid).name mustEqual "Test Project, updated"
+      projectRepository.byId(projectUuid).employees.size mustEqual 1
     }
   }
 
